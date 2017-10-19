adec1b6f9ad7e59987323c828ff453e625b8667e
gutic <agustinterlaak@gmail.com>
2017-10-19 04:00:43 -0300

    funcional

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -16,6 +16,18 @@ namespace WindowsFormsApp1
         public string result;
         public string funcion;
         public string[] terminos;
+        double _Y = 0;
+        double _X = 0;
+        double Yan = 0;
+        double Yant = 0;
+        double Xant = 0;
+        double error = 0.01;
+        bool seguir = true;
+        double Iant = 0;
+        double Fxr = 0;
+        double xr = 0;
+        double resul = 0;
+
         public Form1() 
         {
             InitializeComponent();
@@ -31,21 +43,43 @@ namespace WindowsFormsApp1
             else
             {
                 funcion = (textBox1.Text);
-                
-                maycero(funcion);
+                maycero(funcion); //mando a buscar la diferencia entre negativo y positi
+                Console.WriteLine("Xant " + Xant + ", X, " + _X + ", Yant " + Yant + ", Y " + _Y);
+                Iant = DameiCero(ref _X, ref _Y, ref Xant, ref Yant); //primer xr
+                Console.WriteLine("IANT  " + Iant);
+                Fxr = Funcion_xr(Iant); // vuelvo a buscar en una func similar a maycero
+                Console.WriteLine("Fxr = " + Fxr);
+                while (seguir)
+                {
+                    xr = Damei(ref _X, ref _Y, ref Fxr, ref Iant);
+                    Console.WriteLine("xr " + xr);
+                    resul = Math.Round(((xr - Iant) / xr),4);
+                    if (resul < error)
+                    {
+                        Console.WriteLine("La raiz aproximada es: " + xr);
+                        seguir = false;
+                        break;
+                    }
+                    else
+                    {
+                        Fxr = Funcion_xr(xr);
+                        Iant = xr;
+                    }
+                    
+                }
+
             }
 
                
         }
 
         public void maycero(string funcion)
-        {
-            short signo = 1;
-            int primero = 0;
-            double numero = 0;
+        {    
             string nueva;
             string algo = "";
-            double bufNum = 0;
+
+
+            short signo = 1;
             if (funcion[0] != '-')
             {
                 nueva = funcion.Insert(0, "+");
@@ -56,62 +90,167 @@ namespace WindowsFormsApp1
                 signo *= -1;
             }
             terminos = nueva.Split('x');
-            double bufPot = 0;
-            double acumulador = 0;
 
-            foreach (string termino in terminos)
-            {    
-                if (primero == 1)
-                {
-                    for (int i = 0; i < termino.Length; i++)
+            for(int X=-10; X<11; X++)
+            {
+                double bufPot = 0;
+                double acumulador = 0;
+                int primero = 0;
+                double bufNum = 0;
+                double numero = 0;
+                
+                foreach (string termino in terminos)
+                {    
+                    if (primero == 1)
                     {
-                        if (termino[i] == '^' && i == 0 && termino.Length == 4)
+                        for (int i = 0; i < termino.Length; i++)
                         {
-                            Console.WriteLine("acumulando" + acumulador);
-                            algo = Convert.ToString(termino[i + 1]);
-                            bufPot = Convert.ToDouble(algo);
-                            acumulador += numero * (Math.Pow(2, bufPot));
-                            algo = Convert.ToString(termino[3]);
-                            numero = Convert.ToDouble(algo);
-                            if(termino[2] == '-')
+                            if (termino[i] == '^' && i == 0 && termino.Length == 4)
                             {
-                                numero *= -1;
+                                Console.WriteLine("acumulando" + acumulador);
+                                algo = Convert.ToString(termino[i + 1]);
+                                bufPot = Convert.ToDouble(algo);
+                                acumulador += numero * (Math.Pow(X, bufPot));
+                                algo = Convert.ToString(termino[3]);
+                                numero = Convert.ToDouble(algo);
+                                if(termino[2] == '-')
+                                {
+                                    numero *= -1;
+                                }
                             }
-                        }
-                        
-                        if ( (termino[i] == '+' || termino[i] == '-') && termino.Length == 2)
-                        {
-                            algo = Convert.ToString(termino[1]);
-                            bufNum = Convert.ToDouble(algo);
-                            if(termino[i] == '-')
+                            if ( (termino[i] == '+' || termino[i] == '-') && termino.Length == 2)
                             {
-                                bufNum *= -1;
+                                algo = Convert.ToString(termino[1]);
+                                bufNum = Convert.ToDouble(algo);
+                                if(termino[i] == '-')
+                                {
+                                    bufNum *= -1;
+                                }
+                                Console.WriteLine(" total hasta ahora: " + acumulador);
+                                Console.WriteLine("numero independiente: " + bufNum);
+                                acumulador += bufNum;
+                                numero = X * numero;
+                                Console.WriteLine("ultimo x " + numero);
+                                acumulador += numero;
                             }
-                            Console.WriteLine(" total hasta ahora: " + acumulador);
-                            Console.WriteLine("numero independiente: " + bufNum);
-                            acumulador += bufNum;
-                            numero = 2 * numero;
-                            Console.WriteLine("ultimo x " + numero);
-                            acumulador += numero;
                         }
 
+                        Console.WriteLine("termino");
                     }
-
-                    Console.WriteLine("termino");
+                    if (termino.Substring(1, 1) != "^" && termino.Substring(1, 1) != "+" && termino.Substring(1, 1) != "-" && primero == 0)
+                    {
+                        numero = Convert.ToDouble(termino.Substring(1, 1));
+                        primero = 1;
+                    }
                 }
-                if (termino.Substring(1, 1) != "^" && termino.Substring(1, 1) != "+" && termino.Substring(1, 1) != "-" && primero == 0)
+                Console.WriteLine("X  = |"+ X +"| Y =" + acumulador * signo);
+                if ((((acumulador * signo) * 1) > 0 && Yan < 0))
                 {
-
-                    numero = Convert.ToDouble(termino.Substring(1, 1));
-                    primero = 1;
+                    Xant = X - 1;
+                    _X = X;
+                    _Y = acumulador * signo;
+                    Yant = Yan;
                     
                 }
-
+                Yan = acumulador * signo;
             }
-            Console.WriteLine("resultado" + acumulador*signo);
-            //}
+           
+        } // 1
 
+        public double Funcion_xr(double iant)
+        {
+            double xr = 0;
+            string nueva = "";
+            string algo = "";
+
+
+            short signo = 1;
+            if (funcion[0] != '-')
+            {
+                nueva = funcion.Insert(0, "+");
+            }
+            else
+            {
+                nueva = funcion;
+                signo *= -1;
+            }
+            terminos = nueva.Split('x');
+
+                double bufPot = 0;
+                double acumulador = 0;
+                int primero = 0;
+                double bufNum = 0;
+                double numero = 0;
+
+                foreach (string termino in terminos)
+                {
+                    if (primero == 1)
+                    {
+                        for (int i = 0; i < termino.Length; i++)
+                        {
+                            if (termino[i] == '^' && i == 0 && termino.Length == 4)
+                            {
+                                Console.WriteLine("acumulando" + acumulador);
+                                algo = Convert.ToString(termino[i + 1]);
+                                bufPot = Convert.ToDouble(algo);
+                                acumulador += numero * (Math.Pow(iant, bufPot));
+                                algo = Convert.ToString(termino[3]);
+                                numero = Convert.ToDouble(algo);
+                                if (termino[2] == '-')
+                                {
+                                    numero *= -1;
+                                }
+                            }
+                            if ((termino[i] == '+' || termino[i] == '-') && termino.Length == 2)
+                            {
+                                algo = Convert.ToString(termino[1]);
+                                bufNum = Convert.ToDouble(algo);
+                                if (termino[i] == '-')
+                                {
+                                    bufNum *= -1;
+                                }
+                                Console.WriteLine(" total hasta ahora: " + acumulador);
+                                Console.WriteLine("numero independiente: " + bufNum);
+                                acumulador += bufNum;
+                                numero = iant * numero;
+                                Console.WriteLine("ultimo x " + numero);
+                                acumulador += numero;
+                            }
+                        }
+
+                        Console.WriteLine("termino");
+                    }
+                    if (termino.Substring(1, 1) != "^" && termino.Substring(1, 1) != "+" && termino.Substring(1, 1) != "-" && primero == 0)
+                    {
+                        numero = Convert.ToDouble(termino.Substring(1, 1));
+                        primero = 1;
+                    }
+                }
+                xr = acumulador * signo;
+
+            return Math.Round(xr, 4);
+        } // 3
+
+        public double DameiCero(ref double _X, ref double _Y, ref double Xant, ref double Yant) // 2
+        {
+            double divisor = 0;
+            double dividendo = 0;
+            double resultado = 0;
+            dividendo = (_Y * (Xant - _X));
+            divisor = Yant - _Y;
+            resultado = (_X - (dividendo / divisor));
+            return Math.Round(resultado, 4);
         }
-
+        public double Damei(ref double _X, ref double _Y, ref double Fxr, ref double Iant)
+        {
+            double divisor = 0;
+            double dividendo = 0;
+            double resultado = 0;
+            Console.WriteLine(" _X = " + _X + ", _Y = " + _Y + ", Fxr =" + Fxr + ",Iant = " + Iant);
+            dividendo = (_Y * (Iant - _X));
+            divisor = (Fxr - _Y);
+            resultado = (_X - (dividendo / divisor));
+            return Math.Round(resultado, 4);
+        }
     }
 }
